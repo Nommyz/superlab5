1215759980eb5c2e529dc77b0116d5723bf633ae
lab314-44@labeng.local <lab314-44@labeng.local>
2020-12-15 16:09:52 +0700

    "even odd"

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int n=0,even=0,odd=0;
+    
+	do 
+    {
+           cout << "Enter an integer: ";
+           cin >> n;
+           if(n!=0){
+            	if(n%2==0)
+					{
+                 		even=even+1;
+            		}
+            	else
+            		{
+                 		odd=odd+1;
+            		}
+                }
+    }while(n!=0);
+        
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
+
